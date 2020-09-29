2956651f2591aaaba2cf8a5c8a853ec75312197e
LesliePlays <plays.leslie@gmail.com>
2020-09-29 11:53:54 +0200

    essai mise à jour

--- exo_03.rb
+++ exo_03.rb
@@ -1,3 +1,3 @@
 puts "Bonjour, monde!"
 #puts "Et avec une voix sexy, ça donne: Bonjour, monde!"
-#la ligne 2 ne s'affiche pas, car on l'a mise en commentaire
+#la ligne 2 ne s'affiche pas, parcequ'on l'a mise en commentaire
